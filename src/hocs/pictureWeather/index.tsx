2b61c9a92d9e085b527c2077079c1635c5fbf060
Григорий Цыганков <zigtel@gmail.com>
2022-12-02 02:35:34 +0600

    add fix bg in picture weather in safari

--- src/hocs/pictureWeather/index.tsx
+++ src/hocs/pictureWeather/index.tsx
@@ -44,7 +44,7 @@ const PictureWeatherHok = (style: PictureWeatherHokType)=> {
             position: 'relative',
             width: '100%',
             height: '100%',
-            backgroundColor: 'white',
+            backgroundColor: '#322e36',
           }
         }
       >
